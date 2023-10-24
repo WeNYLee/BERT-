171a4d0393e09e3058d1289679b26d20a9498307
WeNYLee <123932085+WeNYLee@users.noreply.github.com>
2023-10-24 13:52:18 +0900

    Update chinese_example.py

--- Sample codes/hello_world.py
+++ Sample codes/hello_world.py
@@ -1,1 +1,22 @@
-# hello world
+import torch
+from transformers import BertTokenizer
+from IPython.display import clear_output
+
+PRETRAINED_MODEL_NAME = "bert-base-chinese"  # 
+
+# pre-train tokenizer
+tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
+
+clear_output()
+print("PyTorch ver.：", torch.__version__)
+
+
+
+import random
+random_tokens = random.sample(list(vocab), 10)
+random_ids = [vocab[t] for t in random_tokens]
+
+print("{0:20}{1:15}".format("token", "index"))
+print("-" * 25)
+for t, id in zip(random_tokens, random_ids):
+    print("{0:15}{1:10}".format(t, id))
