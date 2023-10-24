e742f4e796aba73d45a577212e825fa860050bfa
WeNYLee <123932085+WeNYLee@users.noreply.github.com>
2023-10-24 13:54:51 +0900

    Create random_token.py

---
+++ Sample codes/random_token.py
@@ -0,0 +1,8 @@
+import random
+random_tokens = random.sample(list(vocab), 10)
+random_ids = [vocab[t] for t in random_tokens]
+
+print("{0:20}{1:15}".format("token", "index"))
+print("-" * 25)
+for t, id in zip(random_tokens, random_ids):
+    print("{0:15}{1:10}".format(t, id))
